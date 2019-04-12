15a0dda2d29ce344c6bc5d93e63d13bc0e859f54
AnuragRatnaIBM <anuragratna00@gmail.com>
2019-04-12 14:39:08 +0530

    changed sout

--- src/main/java/com/stackroute/SpringJenkins/SpringJenkinsApplication.java
+++ src/main/java/com/stackroute/SpringJenkins/SpringJenkinsApplication.java
@@ -8,7 +8,7 @@ public class SpringJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsApplication.class, args);
-		System.out.println("hello world");
+		System.out.println("hello world hahahaah");
 	}
 
 }
